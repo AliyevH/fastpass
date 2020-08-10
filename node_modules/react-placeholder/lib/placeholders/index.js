5731ea39d6c82ca6a2d24809cf54365ad64a7707
Kamil Vahabov <kamil@onneks.com>
2020-08-10 08:49:31 +0400

    version 1.0

---
+++ node_modules/react-placeholder/lib/placeholders/index.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var TextRow_1 = require("./TextRow");
+exports.TextRow = TextRow_1["default"];
+var RoundShape_1 = require("./RoundShape");
+exports.RoundShape = RoundShape_1["default"];
+var RectShape_1 = require("./RectShape");
+exports.RectShape = RectShape_1["default"];
+var TextBlock_1 = require("./TextBlock");
+exports.TextBlock = TextBlock_1["default"];
+var MediaBlock_1 = require("./MediaBlock");
+exports.MediaBlock = MediaBlock_1["default"];
+exports.textRow = TextRow_1["default"];
+exports.round = RoundShape_1["default"];
+exports.rect = RectShape_1["default"];
+exports.text = TextBlock_1["default"];
+exports.media = MediaBlock_1["default"];
